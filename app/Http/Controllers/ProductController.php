c43ef497392d94c7c09eb0f1f0ca3622b67e380e
Markus Wilhelm <wilhelm@mw-systems.com>
2022-07-23 14:16:11 +0200

    implement product model with endpoints

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 use Symfony\Component\HttpFoundation\Response;
@@ -9,22 +10,37 @@ use Symfony\Component\HttpFoundation\Response;
 class ProductController extends Controller
 {
     public function create(Request $request) {
-        Log::debug(__FUNCTION__);
-        return response(null, Response::HTTP_CREATED);
+
+        $product = new Product();
+        $product->title = $request->title;
+        $product->description = $request->description;
+        $product->save();
+
+        return response()->json(['product' => $product], Response::HTTP_CREATED);
     }
 
     public function update($id, Request $request) {
-        Log::debug(__FUNCTION__ . ':' . $id);
-        return response(null, Response::HTTP_OK);
+
+        $product = Product::where('id', $id)->first();
+        $product->title = $request->title;
+        $product->description = $request->description;
+        $product->save();
+
+        return response()->json(['product' => $product], Response::HTTP_OK);
+    }
+
+    public function getProduct($id) {
+        $product = Product::where('id', $id)->first();
+        return response()->json(['product' => $product], Response::HTTP_OK);
     }
 
     public function getAllProducts() {
-        Log::debug(__FUNCTION__);
-        return response(null, Response::HTTP_OK);
+        $products = Product::all();
+        return response()->json(['products' => $products], Response::HTTP_OK);
     }
 
     public function delete($id) {
-        Log::debug(__FUNCTION__ . ':' . $id);
-        return response(null, Response::HTTP_OK);
+        Product::where('id', $id)->delete();
+        return response()->json([], Response::HTTP_OK);
     }
 }
